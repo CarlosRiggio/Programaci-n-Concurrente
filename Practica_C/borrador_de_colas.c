2ebfc2fec8229ef6b9f92d28d2a0579b62e61ee6
CarlosRiggio <criggio02@gmail.com>
2023-05-07 18:57:43 +0200

    creado config nuevo

--- Practica_C/borrador_de_colas.c
+++ Practica_C/borrador_de_colas.c
@@ -1,21 +1,22 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <stdio.h>
-#include <stdlib.h>
+ #include <sys/types.h>
+ #include <sys/ipc.h>
+ #include <sys/msg.h>
+ #include <stdio.h>
+ #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-  int msqid;
-  msqid = atoi(argv[1]);
+ int main(int argc, char *argv[])
+ {
+   int msqid;
+   msqid = atoi(argv[1]);
 
-  // Eliminar la cola de mensajes
-  if (msgctl(msqid, IPC_RMID, NULL) == -1) {
-    perror("msgctl");
-    exit(1);
-  } else {
-    printf("La cola de mensajes con ID %d se ha eliminado\n", msqid);
-  }
+    //Eliminar la cola de mensajes
+   if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+     perror("msgctl");
+     exit(1);
+   } else {
+     printf("La cola de mensajes con ID %d se ha eliminado\n", msqid);
+   }
 
-  return 0;
-}
+   return 0;
+ }
+
